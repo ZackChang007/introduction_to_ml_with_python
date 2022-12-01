2384700f3561081d04ff3dec2eb543efbcd53d79
ZackChang <zhangchunyucoldfish@126.com>
2022-12-01 22:57:25 +0800

    按照原书和官方README.md安装版本，部分版本不可用，这一版拟全部废弃，自己选择最新py版本和包版本

---
+++ py_package_version.py
@@ -0,0 +1,51 @@
+# https://weread.qq.com/web/reader/b0d32910718ff601b0d0aeekaab325601eaab3238922e53
+
+import sys
+
+print("Python version: {}".format(sys.version))
+
+import pandas as pd
+
+print("pandas version: {}".format(pd.version))
+
+import matplotlib
+
+print("matplotlib version: {}".format(matplotlib.version))
+
+import numpy as np
+
+print("NumPy version: {}".format(np.version))
+
+import scipy as sp
+
+print("SciPy version: {}".format(sp.version))
+
+import IPython
+
+print("IPython version: {}".format(IPython.version))
+
+import sklearn
+
+print("scikit-learn version: {}".format(sklearn.version))
+
+""" 
+Out[9]:
+Python
+version: 3.5
+.2 | Anaconda
+4.1
+.1(64 - bit) | (default, Jul  22016, 17:53:06)
+[GCC 4.4.720120313(Red Hat 4.4.7 - 1)]
+pandas
+version: 0.18.1
+matplotlib
+version: 1.5.1
+NumPy
+version: 1.11.1
+SciPy
+version: 0.17.1
+IPython
+version: 5.1.0
+scikit - learn
+version: 0.18
+"""
